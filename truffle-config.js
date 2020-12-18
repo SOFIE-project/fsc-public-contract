28978b2e213444ef382fa9dff494bc5946b55814
Artemios Tomaras <tomaras@synelixis.com>
2020-12-18 16:47:22 +0200

    Added mocha-junit-reporter

--- truffle-config.js
+++ truffle-config.js
@@ -75,7 +75,8 @@ module.exports = {
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
-    // timeout: 100000
+    "reporter": "mocha-junit-reporter"
+  // timeout: 100000
   },
 
   // Configure your compilers
